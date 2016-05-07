a65bf58623d5d652a5b00ef276304591486d0735
rafjordao <rfpj@cin.ufpe.br>
2016-05-07 17:58:31 -0300

    Rollback

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController     
-  before_action :authenticate_user!
-    def  index
-      #@users = User.find_by_sql("select * from tb_users")  
-    
-    end
-end
--- db/migrate/20160430154256_add_nome_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNomeToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :nome, :string
-    add_column :users, :sobrenome, :string
-  end
-end
